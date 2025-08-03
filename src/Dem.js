e5d35920f13b9e1b0555847383afdecf6ceea53d
Govindaraj-web <arjungovi.webcreator@gmail.com>
2025-08-03 20:37:29 +0530

    update router image

---
+++ src/Dem.js
@@ -0,0 +1,3 @@
+const arr= [1,2,3,4,5,6,7,8]
+const newArr = arr.filter(num => num=4)
+console.log(newArr)
--- src/Demo.jsx
+++ src/Demo.jsx
@@ -3,34 +3,33 @@ import { Link } from 'react-router-dom'
 
 function Demo() {
 
-const [user,setUser]=useState([])
+const [users,setUsers] = useState([])
 
-useEffect(()=>{setTimeout(() => {
-  
-
+useEffect(()=> {
   fetch('https://jsonplaceholder.typicode.com/users')
-  .then((response)=>{
+  .then(response => {
     console.log(response)
-    return response.json()
-  }).then((data) =>{
-    setUser(data)}
-  ).catch(error =>console.error("data not fetch:",error))
-  }, 1000);
+    return response.json()})
+  .then(data => setUsers(data))
+  .catch(error => console.error('data not fetch:',error))
 },[])
 
   return (
     <>
       <Link to='/'>Home</Link>  {/* Component Link */}
       <div>
-        <h1>User List Json</h1>
         <ul>
-        {user.map((use)=>(
-          <li key={use.id}>
-            <h2>{use.name}</h2>
-            <h5>{use.username}</h5>
-            <p>{use.email}</p>
-          </li>
-        ))}
+        {users.map((user) => 
+        <li key={user.id}>
+          <strong>Name: {user.name}</strong>
+          <p>Username: {user.username}</p>
+          <p>Email Id: {user.email}</p>   
+          <em>Address</em>
+          <p>Street:{user.address.street}</p>      
+          <p>Zipcode:{user.address.zipcode}</p>
+        </li>
+
+        )}
         </ul>
       </div>
     </>
@@ -39,3 +38,4 @@ useEffect(()=>{setTimeout(() => {
 }
 
 export default Demo
+
